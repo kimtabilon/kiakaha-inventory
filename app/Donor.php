15dd3c8a2cf2351aa7f9bc51cec692324163d912
kim.tabilon <kim.tabilon12345@gmail.com>
2019-01-23 22:44:16 +0800

    Donor created_at formatted date

--- app/Donor.php
+++ app/Donor.php
@@ -14,24 +14,38 @@ class Donor extends Model
     protected $fillable = ['given_name', 'middle_name', 'last_name', 'email'];
     protected $appends  = ['name', 'created', 'modified'];
 
-    public function profile() {     return $this->hasOne('App\Profile'); } 
-    public function inventories() { return $this->belongsToMany('App\Inventory')->withTimestamps(); } 
-    public function donorType() {   return $this->belongsTo('App\DonorType'); } 
-    public function storeCredits() {return $this->hasMany('App\StoreCredit'); } 
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
+    public function inventories()
+    {
+        return $this->belongsToMany('App\Inventory')->withTimestamps();
+    }
+    public function donorType()
+    {
+        return $this->belongsTo('App\DonorType');
+    }
+    public function storeCredits()
+    {
+        return $this->hasMany('App\StoreCredit');
+    }
 
 
     public function getNameAttribute()
     {
-        return preg_replace('/\s+/', ' ',$this->given_name.' '.$this->middle_name.' '.$this->last_name);
+        return preg_replace('/\s+/', ' ', $this->given_name.' '.$this->middle_name.' '.$this->last_name);
     }
 
     public function getCreatedAttribute()
     {
-        return \Carbon\Carbon::parse($this->created_at)->diffForHumans();
+        // return \Carbon\Carbon::parse($this->created_at)->diffForHumans();
+        return \Carbon\Carbon::parse($this->created_at)->toDayDateTimeString();
     }
 
     public function getModifiedAttribute()
     {
-        return \Carbon\Carbon::parse($this->updated_at)->diffForHumans();
+        // return \Carbon\Carbon::parse($this->updated_at)->diffForHumans();
+        return \Carbon\Carbon::parse($this->updated_at)->toDayDateTimeString();
     }
 }
